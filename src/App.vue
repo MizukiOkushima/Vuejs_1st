d62118f4152a7e187a7a2645640635b136da4ad5
MizukiOkushima <m.okushima@fukuinet.co.jp>
2025-01-28 16:37:35 +0900

    20250128_16:30

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@ function increment() {
   instructor.age += 1
   instructor.bio = 'hi'
   instructor.sns.twitter = '@__niseaka__'
+  instructor.email = 'info@example.com'
 }
 const info = ref({
   students: 1000,
@@ -25,14 +26,29 @@ const instructor = reactive({
     twitter: '@__yoshipi__',
     youtube: '@ypshipi',
     a: {}
-  }
+  },
+  email: ref('yoshipi@example.com')
 })
 instructor.bio = 'hello'
+const items = reactive([ref(1), ref(2), ref(3)])
+// items.reverse()
+// const courseInfo = {
+//   sections: ref(10),
+//   language: ref('Japanese')
+// }
+// const sections = courseInfo.sections
+// console.log(courseInfo.sections.value)
+const { sections } = {
+  sections: ref(10),
+  language: ref('Japanese')
+}
+// console.log(items[0].value)
 // console.log(instructor.age)
 // console.log(info)
-console.log(info.value)
+// console.log(info.value)
 // console.log(instructor)
-console.log(instructor.sns)
+// console.log(instructor.sns)
+// console.log(instructor.email)
 </script>
 <template>
   <h1>Title: {{ title }}</h1>
@@ -42,6 +58,9 @@ console.log(instructor.sns)
   <h2>instructor age:{{ instructor.age }}</h2>
   <h2>instructor bio:{{ instructor.bio }}</h2>
   <h2>instructor SNS Twitter:{{ instructor.sns.twitter }}</h2>
+  <h2>instructor email:{{ instructor.email }}</h2>
+  <!-- <h2>Course Info Sections:{{ courseInfo.sections.value }}</h2> -->
+  <h2>Course Info Sections:{{ sections + 1 }}</h2>
 </template>
 <style>
 h1 {
